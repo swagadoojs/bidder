b1ec9dcfb16035d84eda297773819e4e8f63abb0
Jimmy Sander <jimmy@swagadoo.com>
2013-05-01 10:27:50 -0400

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bidder::Application.config.secret_token = 'f1a8cee795e7a6c28104f9e55179ac65bbb6f8326de7cbeb86cc695dc79495c4216f10ccf2da51a241c47af6ddfce567ff7c934b32ab181c7805273e080fe099'
